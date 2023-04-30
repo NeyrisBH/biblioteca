975fc4df095d3f8a77928aedd39deeb9075ad953
Neyris Bohorquez <luz.bohorquez2@o365.unab.edu.co>
2023-04-30 18:58:26 -0500

    build

--- src/main/java/com/ucompensar/biblioteca/Biblioteca.java
+++ src/main/java/com/ucompensar/biblioteca/Biblioteca.java
@@ -1,11 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.ucompensar.biblioteca;
-
 /**
- *
  * @author Neyris
  */
 public class Biblioteca {
