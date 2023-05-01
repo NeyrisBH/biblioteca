e56afa5e3067d37a4228589f46cb2af7ef386298
Neyris Bohorquez <luz.bohorquez2@o365.unab.edu.co>
2023-04-30 19:43:02 -0500

    Create Libros.java

---
+++ src/main/java/Clases/Libros.java
@@ -0,0 +1,10 @@
+package Clases;
+/**
+ * @author Neyris
+ */
+public class Libros {
+    private int codigoLibro, numeroPaginas;
+    private String nombreLibro, autorLibro, materia;
+    
+    
+}
